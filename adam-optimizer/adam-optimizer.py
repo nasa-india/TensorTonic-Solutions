2cedfae5f7a46018c8e027d5e58b73e2b563170e
pranathi <pranathiparvathaneni@gmail.com>
2026-09-18 19:46:30 +0530

    Solve adam-optimizer

--- adam-optimizer/adam-optimizer.py
+++ adam-optimizer/adam-optimizer.py
@@ -11,22 +11,24 @@ def adam_step(
     beta2: float = 0.999,
     eps: float = 1e-8,
 ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-    """
-    //Returns (param_new, m_new, v_new) as NumPy arrays.
-    """
 
     # Write code here
-    param=np.array(param)
+    param=np.array(param)#convert input list into numpy arrays
     grad=np.array(grad)
     m=np.array(m)
     v=np.array(v)
 
-    m_new=beta1*m+(1-beta1)*grad
-    v_new=beta2*v+(1-beta2)*grad**2
-    m_hat=m_new/(1-beta1**t)
-    v_hat=v_new/(1-beta2**t)
+    m_new=beta1*m+(1-beta1)*grad  #update first moment
+    v_new=beta2*v+(1-beta2)*grad**2  #update biased second raw moment
+    m_new1=m_new/(1-beta1**t)  #compute biased correct first moment
+    v_new2=v_new/(1-beta2**t)  #xomputr biased correctd second raw momnet
 
-    param_new=param-(lr*m_hat)/(np.sqrt(v_hat)+eps)
-    return param_new,m_new,v_new
+    
+    """
+    Returns (param_new, m_new, v_new) as NumPy arrays.
+    """
+
+    param_new=param-(lr*m_new1)/(np.sqrt(v_new2)+eps)  #update params
+    return param_new,m_new,v_new 
     
     
